13ceca6951479edc6f61a24f316a3796065e0dba
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-03-03 14:21:43 +0100

    --marketplace set up

--- primero-marketplace/tailwind.config.js
+++ primero-marketplace/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
