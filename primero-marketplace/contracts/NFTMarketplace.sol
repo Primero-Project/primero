099b564164d86fe1ccb7108771499078a1787643
Fatma Aq <abdulqadirfatma@gmail.com>
2022-03-03 23:56:36 +0300

    token name

--- primero-marketplace/contracts/NFTMarketplace.sol
+++ primero-marketplace/contracts/NFTMarketplace.sol
@@ -33,7 +33,7 @@ contract NFTMarketplace is ERC721URIStorage {
       bool sold
     );
 
-    constructor() ERC721("Metaverse Tokens", "METT") {
+    constructor() ERC721("Primero Token", "PRMT") {
       owner = payable(msg.sender);
     }
 
