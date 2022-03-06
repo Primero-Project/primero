27a43cddb48cbf15ba06566750acac65aa688c14
Fatma Aq <abdulqadirfatma@gmail.com>
2022-03-06 17:23:14 +0300

    UPDATE hardhat.config.js networks

--- primero-marketplace/hardhat.config.js
+++ primero-marketplace/hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+//dont a key to your main accounts
+const RINKEBY_KEY = "3af5eaccf2c413d190b63e64c86b26e38c1a5f502d8ac780ef5184ed12f2b0e4"
+
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -8,13 +11,18 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
-    mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [process.env.privateKey]
+    rinkeby: {
+      //url:https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}
+      url: `https://eth-rinkeby.alchemyapi.io/v2/eRPK-XANyIankxAbgOD_YIDYZxeqDD_f`,
+      accounts: [RINKEBY_KEY]
     },
+    /*
+     /*mumbai: {
+      // Alchemy
+      // url: `https://polygon-mumbai.g.alchemy.com/v2/${AlchemyId}`
+      url: "https://polygon-mumbai.g.alchemy.com/v2/eRPK-XANyIankxAbgOD_YIDYZxeqDD_f",
+      accounts: [`${MUMBAI_KEY}`]
+    },*/
     matic: {
       // Infura
       // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
