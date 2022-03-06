272542072c25937de924af961dd2097bde9bc4cf
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-03-06 23:22:46 +0100

    -- deploy

--- primero-marketplace/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/course":["static\u002Fchunks\u002Fpages\u002Fcourse.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcourse"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
